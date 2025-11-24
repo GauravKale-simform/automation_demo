cc659a6340755b7c9bf91222edced821a3697b8d
Gaurav Kale <gaurav.kale@simformsolutions.com>
2025-11-24 18:31:15 +0530

    changes

--- test_cases/test_inventory.py
+++ test_cases/test_inventory.py
@@ -6,12 +6,10 @@ from POM.cart_page import CartPage
 from POM.product_page import ProductPage
 from selenium.webdriver.common.by import By
 
-
 def login(driver):
     login_page = LoginPage(driver)
     login_page.load()
     login_page.login("standard_user", "secret_sauce")
-
 
 def test_add_to_cart(driver):
     login(driver)
@@ -22,14 +20,12 @@ def test_add_to_cart(driver):
     assert cart.is_loaded()
     assert cart.get_cart_item_name() != ""
 
-
 def test_remove_from_cart(driver):
     login(driver)
     inventory = InventoryPage(driver)
     inventory.add_first_product()
     inventory.remove_first_product()
     assert "Add to cart" in inventory.find(inventory.FIRST_ADD_TO_CART).text
-
 
 def test_sort_products(driver):
     login(driver)
@@ -38,8 +34,6 @@ def test_sort_products(driver):
     first_item_text = inventory.find((By.CLASS_NAME, "inventory_item_name")).text
     assert first_item_text[0] in "ZYXWVUTSRQPONMLKJIHGFEDCBA"
 
-
-
 def test_open_product_details(driver):
     login(driver)
     inventory = InventoryPage(driver)
@@ -47,7 +41,6 @@ def test_open_product_details(driver):
     inventory.open_first_product()
     product = ProductPage(driver)
     assert product.get_title() == product_name
-
 
 def test_logout(driver):
     login(driver)
--- test_cases/test_login.py
+++ test_cases/test_login.py
@@ -6,15 +6,12 @@ from POM.inventory_page import InventoryPage
 def test_valid_login(driver):
     login_page = LoginPage(driver)
     inventory_page = InventoryPage(driver)
-
     login_page.load()
     login_page.login("standard_user", "secret_sauce")
     assert inventory_page.is_loaded() is True
 
 def test_invalid_login(driver):
     login_page = LoginPage(driver)
-
     login_page.load()
     login_page.login("wrong_user", "wrong_pass")
-
     assert "Epic sadface" in login_page.get_error()
